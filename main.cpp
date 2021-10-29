313c8bb02a731b63da7c647cc229c9ab4413f3d6
Ira <vashenko1607@gmail.com>
2021-10-29 07:46:24 +0300

    added files

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "functions.h"
+
+int main() {
+    print_hello();
+    std::cout << "this is" <<std::endl;
+    std::cout << "The factorial of S is" << factorial(5) << std::endl;
+    return 0;
+    
+}
